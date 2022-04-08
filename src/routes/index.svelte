985224a583e0a7a1b9dc5e31d3e3e87d8813bf59
LeoDog896 <LeoDog896@hotmail.com>
2022-04-08 15:16:57 +0000

    Colors!

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,45 @@
 <script lang="ts">
-    import { Engine, Render, Runner, Bodies, Composite, Body, Events, World } from "matter-js"
+    import decomp from 'poly-decomp';
+    import { Engine, Runner, Bodies, Composite, Common, Body, Events, World } from "matter-js"
     import { onMount } from "svelte";
+    import randomcolor from "randomcolor"
 
     let character;
     let engine;
     let ground;
-    let score = 0;
+    let width = 800
+    let height = 600
+    let canvas: HTMLCanvasElement;
+
+    function newGround() {
+        if (ground) World.remove(engine.world, ground)
+        ground = Bodies.rectangle(0, height, width * 4, 60, { isStatic: true });
+        Composite.add(engine.world, ground)
+    }
 
     const randomNumber = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-    let elements = []
-
     onMount(() => {
+        
+        width = window.innerWidth;
+        height = window.innerHeight;
+
         // create an engine
         engine = Engine.create();
 
-        // create a renderer
-        const render = Render.create({
-            element: document.body,
-            engine: engine
-        });
+        Common.setDecomp(decomp)
 
         // create two boxes and a ground
-        character = Bodies.rectangle(400, 200, 80, 80);
-        ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-        // add all of the bodies to the world
-        Composite.add(engine.world, [character, ground]);
-
-        // run the renderer
-        Render.run(render);
+        character = Bodies.fromVertices(400, height - 60, [[
+            // bottom
+            {x: 0, y: 160}, {x: 80, y: 160}, 
+            {x: 100, y: 50}, {x: 60, y: 80},
+            {x: 20, y: 80}, {x: -20, y: 50},
+            {x: 0, y: 160}
+        ]]);
 
         // create runner
         const runner = Runner.create();
-
-        // run the engine
-        Runner.run(runner, engine);
 
         type BodyPair = { bodyA: any, bodyB: any }
 
@@ -46,6 +50,14 @@
             if (pair.bodyB != body) return pair.bodyB
         }
 
+        // run the engine
+        Runner.run(runner, engine);
+
+        newGround()
+
+        // add all of the bodies to the world
+        Composite.add(engine.world, [character]);
+
         Events.on(engine, 'collisionStart', function(event) {
 
             event.pairs.forEach(pair => {
@@ -55,37 +67,58 @@
                     return
                 }
 
-                if (allNotEquals(pair, character)) return
-
-                if (allNotEquals(pair, ground)) {
-                    score++
-                }
-
             })
         });
 
         Events.on(engine, 'collisionEnd', function(event) {
-            event.pairs.forEach(pair => {
-                if (allNotEquals(pair, character)) return
-
-                score--
-            })
+            
         });
 
         Body.setInertia(character, Infinity);
 
         setInterval(() => {
+            if (!document.hasFocus()) return
 
-            const currentPosition = character.position
-
-            const randomX = randomNumber(currentPosition.x - 200, currentPosition.x + 200)
+            const randomX = randomNumber(0, width)
 
             const randomRectangle = Bodies.rectangle(randomX, 200, 10, 10)
 
-            elements += randomRectangle
-
             Composite.add(engine.world, randomRectangle)
-        }, 5000)
+        }, 200)
+
+        const context = canvas.getContext("2d");
+
+        (function render() {
+            const bodies = Composite.allBodies(engine.world);
+
+            window.requestAnimationFrame(render);
+
+            context.fillStyle = '#fff';
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            for (const body of bodies) {
+
+                for (const part of body.parts) {
+                    const vertices = part.vertices;
+                    context.beginPath();
+
+                    context.moveTo(vertices[0].x, vertices[0].y)
+
+                    for (const vertex of vertices) {
+                        context.lineTo(vertex.x, vertex.y);
+                    }
+
+                    context.lineTo(vertices[0].x, vertices[0].y);
+
+                    context.closePath();
+                    if (body == character) {
+                        context.fillStyle = randomcolor({ seed: part.id })
+                    } else
+                        context.fillStyle = randomcolor({ seed: body.id })
+                    context.fill();
+                }
+            }
+        })();
     })
 
     function input(event: KeyboardEvent) {
@@ -99,5 +132,10 @@
     }
 </script>
 
-<p class="fixed m-8 text-white">{score}</p>
-<svelte:window on:keydown={input}></svelte:window>
+<canvas width={width} height={height} bind:this={canvas}></canvas>
+<svelte:window on:resize={() => {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    ground.width = width;
+    newGround()
+}} on:keydown={input}></svelte:window>
